00e42c154a65d5dbafd5e6d8b0db02968d56bb10
arsenoliinyk <arsen.oliynyk@gmail.com>
2021-05-13 22:53:37 +0300

    Modified .gitignore and App.java

--- src/main/java/ua/lviv/iot/App.java
+++ src/main/java/ua/lviv/iot/App.java
@@ -14,24 +14,24 @@ public class App
     {
         DeviceManager manager = new DeviceManager();
 
-        Devices device_1 = new DevicesOfMechanics(500, "сплав Скло", Country.GERMANY,
+        Devices firstDevice = new DevicesOfMechanics(500, "сплав Скло", Country.GERMANY,
                 Equipment.FOR_DEMONSTRATION, 300, DeviceType.MECHANICS, "Not so well");
-        Devices device_2 = new DevicesOfElectricityAndMagnetism(6000, "сплав Скло", Country.CHINA,
+        Devices secondDevice = new DevicesOfElectricityAndMagnetism(6000, "сплав Скло", Country.CHINA,
                 Equipment.FOR_LABORATORY, 1500, DeviceType.ELECTRICITY_AND_MAGNETISM, Sensivity.MEDIUM);
-        Devices device_3 = new DevicesOfElectricityAndMagnetism(8999, "сплав Скло", Country.CHINA,
+        Devices thirdDevice = new DevicesOfElectricityAndMagnetism(8999, "сплав Скло", Country.CHINA,
                 Equipment.FOR_LABORATORY,1899, DeviceType.ELECTRICITY_AND_MAGNETISM, Sensivity.LIGHT);
-        Devices device_4 = new DevicesOfMolecularPhysicsAndThermodynamics(7000, "сплав Скло", Country.GERMANY,
+        Devices fourthDevice = new DevicesOfMolecularPhysicsAndThermodynamics(7000, "сплав Скло", Country.GERMANY,
                 Equipment.FOR_DEMONSTRATION, 8500, DeviceType.MOLECULAR_PHYSICS_AND_THERMODYNAMICS,
                 "Enough for demonstrations");
-        Devices device_5 = new DevicesOfOptics(200, "сплав Скло", Country.CHINA, Equipment.FOR_DEMONSTRATION,
+        Devices fifthDevice = new DevicesOfOptics(200, "сплав Скло", Country.CHINA, Equipment.FOR_DEMONSTRATION,
                 800, DeviceType.OPTICS, "Easy to replace");
 
 
-        manager.addDevice(device_1);
-        manager.addDevice(device_2);
-        manager.addDevice(device_3);
-        manager.addDevice(device_4);
-        manager.addDevice(device_5);
+        manager.addDevice(firstDevice);
+        manager.addDevice(secondDevice);
+        manager.addDevice(thirdDevice);
+        manager.addDevice(fourthDevice);
+        manager.addDevice(fifthDevice);
 
         manager.printManagersList(manager.searchByType(DeviceType.MECHANICS));
         System.out.println("--------------------------------------------------------------------------------------");
